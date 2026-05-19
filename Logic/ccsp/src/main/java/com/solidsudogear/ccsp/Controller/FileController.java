2bdfc2fb1d348a1ef056ab2a74c9ac8b10beebdf
RaHQ_05 <Rawihk2005@gmail.com>
2026-05-19 19:26:58 +0300

    added controller

---
+++ Logic/ccsp/src/main/java/com/solidsudogear/ccsp/Controller/FileController.java
@@ -0,0 +1,18 @@
+package com.solidsudogear.ccsp.Controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/file")
+public class FileController {
+    
+
+    @GetMapping
+    public ResponseEntity<String> confirming(){
+        return ResponseEntity.ok("request arrived");
+    }
+}
+
