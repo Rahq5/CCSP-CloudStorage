8c8a3c1312190db19d59b1842fe527e0908048dc
RaHQ_05 <Rawihk2005@gmail.com>
2026-07-25 23:11:48 +0300

    uploading done

--- Logic/ccsp/src/main/java/com/solidsudogear/ccsp/Controller/UploadExpController.java
+++ Logic/ccsp/src/main/java/com/solidsudogear/ccsp/Controller/UploadExpController.java
@@ -1,5 +1,12 @@
 package com.solidsudogear.ccsp.Controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -45,6 +52,35 @@ public class UploadExpController {
     @GetMapping("/Hello")
     public String greetings() {
         return "hello";
+    }
+
+    // uploading files
+    @PostMapping(value="/upload")
+    public ResponseEntity<?> uploadFile(@RequestParam MultipartFile file){
+       
+        // making directory and saving it to files object so each file know where to be saved  
+        String dir ="uploads";
+        File directory = new File(dir);
+
+        // check if file exist
+        if(!directory.exists()){
+            directory.mkdir();
+        }
+
+        // try-catch block in case of any file uploading failure , if failed it prints the problem and returns badRequest
+        try {
+
+            String filename = UUID.randomUUID()+ "  "+file.getOriginalFilename();
+            Path filePath =  Paths.get(dir +File.separator+ filename);
+            Files.write(filePath , file.getBytes());
+            return ResponseEntity.ok("file is uploaded and stored:" + file.getOriginalFilename());
+
+        } catch (IOException e) {
+          e.printStackTrace();
+          return ResponseEntity.badRequest().build();
+        }
+        
+
     }
     
     
